bc41ee52896e893be3fd8649ed70376bcac49d9a
Liana Principe <69564111+lap309@users.noreply.github.com>
2020-09-24 21:03:07 -0400

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,12 @@
+##Load the packages we will be using and the data
+library(dplyr)
+NEI<-readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+new_NEI<- NEI                                           ##to keep raw data untouched
+new_NEI$yeardate<- as.factor(NEI$year)
+
+##Graph1: Have total emissions decreased in the US?
+sum1<-new_NEI %>% group_by(yeardate) %>% summarize(n=n(), Total=sum(Emissions))
+plot(sum1$yeardate, sum1$Total, type="l", xlab="Year", ylab="Total Emissions in US")
+dev.copy(png,file="plot1.png")
+dev.off()
