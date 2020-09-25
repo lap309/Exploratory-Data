83816dbf151dc1badf8a18febb7d2940358b6493
Liana Principe <69564111+lap309@users.noreply.github.com>
2020-09-24 21:12:49 -0400

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -10,3 +10,5 @@ sum1<-new_NEI %>% group_by(yeardate) %>% summarize(n=n(), Total=sum(Emissions))
 plot(sum1$yeardate, sum1$Total, type="l", xlab="Year", ylab="Total Emissions in US")
 dev.copy(png,file="plot1.png")
 dev.off()
+
+##Using the graph, we can see that US emissions of PM2.5 have decreased since 1999
